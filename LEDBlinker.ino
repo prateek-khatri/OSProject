d93de81f85ac8362b4ce6d3fc9b8097f6a9d421a
Prateek Khatri <prateek.khatri90@gmail.com>
2015-11-24 22:19:37 -0800

    Create LEDBlinker.ino

---
+++ LEDBlinker.ino
@@ -0,0 +1,88 @@
+int jobSize = 200;
+int algoType;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+  //Serial.println("Waiting for Algo Type");
+  while(!(Serial.available() >0));
+  algoType = Serial.read();
+  Serial.println(jobSize);
+  Serial.flush();
+  //Serial.println("Wating for Start Signal");
+  while(!(Serial.available() >0));
+  if(Serial.read() == 'g')
+  {
+    Serial.flush();
+    return;
+  }
+  else Serial.println("ERROR");
+
+}
+  
+  
+  
+
+
+void waitForStart()
+{
+  //Serial.println("Waiting for Start");
+  if(algoType == '1')
+  {
+  Serial.println(jobSize);
+  }
+  while(!(Serial.available() > 0));
+  if(Serial.read() == 'g')
+  return;
+}
+
+void signalPolling()
+{
+  if(Serial.available() > 0)
+  {
+    if(Serial.read() == 's')
+    {
+      //Serial.println("Stopped");
+      waitForStart();
+    }
+  }
+  else return;
+  
+}
+
+void loop()
+{
+  
+    digitalWrite(13,HIGH);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    digitalWrite(13,LOW);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    delay(100);
+    signalPolling();
+    jobSize-=10;
+    if(jobSize<=0)
+    {
+      Serial.println("e");
+      while(true);
+      
+    }
+}
